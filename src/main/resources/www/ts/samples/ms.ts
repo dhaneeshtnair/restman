5e99b2e5df2a0d56ce32d9af573bbb6185b0a46c
Dhaneesh <dhaneeshtnair@gmail.com>
2019-04-25 15:30:09 +0530

    changed

--- src/main/resources/www/ts/samples/ms.ts
+++
@@ -1,6 +0,0 @@
-/////////Start Params
-var params = {}
-//Donot delete this block. It is mandatory to have this with every script.
-//Params will automatically removed while saving the script.
-//Params can be provided as a runtime object while invoking the script.
-/////////End Params
